65372b753e783fae3903f744ff5c72ca2407e0ef
barbaradsp <barbaradspimenta@gmail.com>
2025-06-09 08:16:12 -0300

    Creating menu with background, sound and text

--- code/Game.py
+++ code/Game.py
@@ -1,20 +1,15 @@
 import pygame
 
+from code.Const import *
 from code.Menu import Menu
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(800, 600))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
-
-            # Check for all events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()  # Close game window
-                    quit()  # End pygame
